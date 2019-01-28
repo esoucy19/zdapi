a69dcea83eb1a98dd634d8f213733443c120e2d0
Etienne Soucy <esoucy@bigtek.ca>
2019-01-28 16:49:36 -0500

    Cleaned up code and added tests

--- api.py
+++ api.py
@@ -1,77 +1,53 @@
 """
-This module queries Zoho Desk's API obtain lists of clients, tickets, etc.
+This module queries Zoho Desk's API to obtain lists of clients, tickets, etc.
 """
 import requests
+import os
 import itertools
 
 api_root = 'https://desk.zoho.com/api/v1/'
+token = os.environ.get('ZOHO_DESK_API_TOKEN')
+org_id = os.environ.get('ZOHO_DESK_API_ORG_ID')
 
 
-def get(endpoint, token, org_id='', headers={}):
-    """
-    This is our main HTTP get function.
+def get(endpoint, *args, **kwargs):
+    url = api_root + endpoint
+    auth = 'Zoho-authtoken ' + token
 
-    Takes the following parameters:
+    if kwargs.get('headers') is None:
+        kwargs['headers'] = dict()
+    kwargs['headers']['orgId'] = org_id
+    kwargs['headers']['Authorization'] = auth
 
-    endpoint: String, the API endpoint to use, i.e. 'accounts?from=0&limit=99'
-    token: String, the auth token to provide
-    org_id: String, id of the organization to query. Usually required except
-            when querying for organizations.
-    headers: Dict, additionnal headers to provide if needed
-
-    Returns an HTTP response object
-
-    example:
-    response = api.get(endpoint='accounts?from=0&limit=99',
-                       token=token,
-                       org_id=org_id)
-    """
-    url = ''.join([api_root, endpoint])
-    auth = ''.join(['Zoho-authtoken ', token])
-
-    request_headers = dict(headers, Authorization=auth, orgID=org_id)
-
-    response = requests.get(url, headers=request_headers)
+    response = requests.get(url, *args, **kwargs)
     if (response.raise_for_status() is None):
         return response
 
 
-def get_accounts(token, org_id, start, limit):
-    """
-    Returns a list of dict objects containing all accounts for a given org_id
-    and range, starting at 'start' and ending at 'limit'.
-    """
-    endpoint = ''.join(['accounts?from=', str(start),
-                        '&limit=', str(limit)])
-
-    response = get(endpoint=endpoint,
-                   token=token,
-                   org_id=org_id)
+def list_accounts(start, limit):
+    endpoint = 'accounts'
+    params = {'from': start,
+              'limit': limit}
+    response = get(endpoint, params=params)
 
     accounts = response.json()['data']
     return accounts
 
 
-def get_all_accounts(token, org_id):
-    """
-    Returns a list of dict objects containing all accounts for a given org_id.
-    """
-
-    def accounts_generator(token, org_id):
+def get_all_accounts():
+    def accounts_generator():
         for start in itertools.count(0, 99):
             limit = 99
-            accounts = get_accounts(token, org_id, start, limit)
+            accounts = list_accounts(start, limit)
             yield accounts
 
     def accounts_len_99(accounts):
-        """
-        Gets a list of accounts and returns of tuple containing the list of
-        accounts and its length.
-        """
         return len(accounts) == 99
 
     accounts = sum(itertools.takewhile(accounts_len_99,
-                                       accounts_generator(token, org_id)), [])
+                                       accounts_generator()), [])
     return accounts
 
 
+def cache_accounts():
+    pass
--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 import api
+
+
+def test_get_organizations():
+    r = api.get('organizations')
+    assert r.status_code == 200
+    assert api.org_id in r.text
+
+
+def test_list_accounts():
+    account = api.list_accounts(start=1, limit=1)[0]
+    assert account['accountName']
+
+
+def test_get_all_accounts():
+    accounts = api.get_all_accounts()
+    assert len(accounts) > 100
